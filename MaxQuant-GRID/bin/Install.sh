8f5d4eae95978cc5017332fbc2178194462bcf7e
Andrew Collins <detritus@users.noreply.github.com>
2021-06-22 11:16:55 +0100

    Added missing Install scripts

---
+++ MaxQuant-GRID/bin/Install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+INSTALL_DIR=`pwd`/../bin
+INSTALL_DIR=`realpath $INSTALL_DIR`
+RESET=`tput sgr0`
+GOOD=`tput setaf 2`
+BAD=`tput setaf 1`
+DOTNET_FOUND="N"
+
+printf "Evironment information\n"
+printf "Command dotnet available: "
+if command -v dotnet &> /dev/null; then
+    DOTNET_VERSION=`dotnet --version`
+    DOTNET_VERSION=${DOTNET_VERSION:0:3}
+    
+    if [ $DOTNET_VERSION != "2.1" ] && [ $DOTNET_VERSION != "2.2" ]; then
+        printf "$BAD ERROR: Incompatible dotnet version detected. Expected 2.1 or 2.2, found $DOTNET_VERSION $RESET\n"
+    else
+        printf "$GOOD $DOTNET_VERSION FOUND $RESET\n"
+    fi
+
+else
+    printf "$BAD ERROR: dotnet command not found $RESET\n"
+fi
+
+printf "Command singularity available: "
+if command -v singularity &> /dev/null; then
+    printf "$GOOD OK $RESET\n"
+else
+    printf "$BAD ERROR: singularity command not found $RESET\n"
+fi
+
+printf "Dotnet capable MaxQuant: "
+if [ -f "$INSTALL_DIR/MaxQuantTaskCore.dll" ]; then
+    printf "$GOOD OK $RESET\n"
+    DOTNET_FOUND=Y
+else
+    printf "$BAD ERROR: MaxQuant not dotnet capable $RESET\n"
+fi
+
+if [ $DOTNET_FOUND == N ]; then
+    printf "$BAD FATAL: Cannot continue. MaxQuant not found. Likely cause is MaxQuant-GRID has been extracted to the wrong location. It should be installed in the MaxQuant directory next to bin$RESET\n"
+    printf "./MaxQuant\n"
+    printf "./MaxQuant/bin\n"
+    printf "$GOOD./MaxQuant/GRID$RESET\n"
+    printf "./MaxQuant/MaxQuant.exe\n"
+    exit 1
+fi
+
+printf "\n"
+printf "Install [y/n]: "
+read DO_INSTALL
+
+if [ $DO_INSTALL != "y" ] && [ $DO_INSTALL != "Y" ]; then
+    printf "$BAD Unable to proceed without user input $RESET\n"
+    exit 1
+fi
+
+printf "Installing for dotnet: "
+
+mv $INSTALL_DIR/MaxQuantTaskCore.dll $INSTALL_DIR/MaxQuantTaskCore.Original.dll
+mv $INSTALL_DIR/MaxQuantTaskCore.runtimeconfig.json $INSTALL_DIR/MaxQuantTaskCore.Original.runtimeconfig.json
+
+cp ../Newtonsoft.Json.dll $INSTALL_DIR
+cp ../RabbitMQ.Client.dll $INSTALL_DIR
+cp ../System.Threading.Channels.dll $INSTALL_DIR
+cp ../MaxQuantTaskCore.dll $INSTALL_DIR
+cp ../MaxQuantTaskCore.deps.json $INSTALL_DIR
+cp ../MaxQuantTaskCore.runtimeconfig.json $INSTALL_DIR
+
+sed -i -e '/runtimepath=/d' ../config/default.conf
+sed -i -e '/taskpath=/d' ../config/default.conf
+
+echo "runtimepath=dotnet" >> ../config/default.conf
+echo "taskpath=MaxQuantTaskCore.Original.dll" >> ../config/default.conf
+printf "$GOOD OK $RESET\n"
+
+printf "To create the RabbitMQ instance, now excute the comamnd: \n"
+
+printf "singularity build --remote rabbitmq/rabbitmq.sif rabbitmq/rabbitmq.def\n"
+
+printf "You may be required to login for this to work.\n"
